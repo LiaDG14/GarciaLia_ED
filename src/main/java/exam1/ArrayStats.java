70cc6273a032ea90af708a8eb354c1f667aad172
LiaDG14 <garcialiadamaris@gmail.com>
2023-11-20 17:21:22 +0100

    EX1: ArrayStats maxim

--- src/main/java/exam1/ArrayStats.java
+++ src/main/java/exam1/ArrayStats.java
@@ -1,2 +1,30 @@
-package exam1;public class ArrayStats {
-}
+package exam1;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ArrayStats {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Introdueix el nombre d'elements de l'array:");
+        int n = in.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Introdueix el contingut de l'array:");
+        for(int i = 0; i < n; i++){
+            array[i] = in.nextInt();
+        }
+
+        int maxim = Integer.MIN_VALUE;
+        for(int i = 0; i < array.length; i++){
+            if(maxim < array[i]){
+                maxim = array[i];
+          }
+      }
+
+
+        System.out.printf("L'element més gran és: %d\n", maxim);
+        }
+    }
+
