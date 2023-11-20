4993edff9104f8c5cf79ee43f24c449d0d951183
LiaDG14 <garcialiadamaris@gmail.com>
2023-11-20 16:27:12 +0100

    exam1

---
+++ src/main/java/exam1/WhichPizzaShouldIBuy.java
@@ -0,0 +1,25 @@
+package exam1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class WhichPizzaShouldIBuy {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Introdueix el diàmetre de la pizza redona:");
+        double diametre = in.nextDouble();
+
+        System.out.println("Introdueix els dos costats de la pizza rectangular:");
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+
+        double areaRedona = 2 * Math.PI * diametre;
+        double areaRectangular = a + b;
+
+        if (areaRedona < areaRectangular)
+            System.out.println("Compra la pizza redona!");
+        else
+            System.out.println("Compra la pizza rectangular!");
+    }
+}
--- src/main/java/ud2/practices/MultiplyTableFull.java
+++ src/main/java/ud2/practices/MultiplyTableFull.java
@@ -1,4 +1,4 @@
-ºpackage ud2.practices;
+package ud2.practices;
 
 import java.util.Scanner;
 import java.util.Locale;
@@ -6,11 +6,11 @@ import java.util.Locale;
 public class MultiplyTableFull {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
-+       int n = in.nextInt();
+        int n = in.nextInt();
 
 
         for (int i = 1; i <= 10; i++) {
-+ for (int j = 1; j <= 20; j++) {            
+ for (int j = 1; j <= 20; j++) {
                 System.out.printf(" %3d", i*j);
             }
             System.out.println();
